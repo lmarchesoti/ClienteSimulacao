6bc8a0756370d07ef01b14c30af5e6ab2ca840ae
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2026-02-20 16:14:51 -0300

    Initial commit

---
+++ src/main/java/dev/lmarchesoti/ClienteSimulacao/ClienteSimulacaoApplication.java
@@ -0,0 +1,13 @@
+package dev.lmarchesoti.ClienteSimulacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteSimulacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteSimulacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lmarchesoti/ClienteSimulacao/ClienteSimulacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lmarchesoti.ClienteSimulacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteSimulacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
